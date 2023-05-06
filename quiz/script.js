2b4e5e0cf8b25e257782749fd30f550260dca873
doublejumpz <zumbivirtual00@gmail.com>
2023-05-05 21:13:23 -0300

    second commit

--- quiz/script.js
+++ quiz/script.js
@@ -1,3 +1,4 @@
+// banco de dados (perguntas)
 const questions = [
   {
     question: "qual a capital da França?",
@@ -30,10 +31,57 @@ const questions = [
   }, //objeto
 ];
 
-const timer = document.querySelector(".time-left");
+// question container
 const question = document.querySelector(".question");
-const questioText = document.querySelector(".question-text");
-
-
-
+const questionText = document.querySelector(".question-text");
 const answerOptions = document.querySelectorAll("input[type='radio']");
+// timer container
+const timer = document.querySelector(".time-left");
+// button container
+const submitButton = document.querySelector("#submit-button");
+const nextButton = document.querySelector("#next-button");
+const restartButton = document.querySelector("#restart-button");
+// result container
+const resultContainer = document.querySelector(".result-container");
+const result = document.querySelector(".result");
+
+let currentQuestion = 0;
+let score = 0;
+let timeLeft = 10;
+let intervalId;
+
+// functions sequence
+
+// time function
+const setTimeLeft = () => {
+  timeLeft--;
+  timer.textContent = timeLeft;
+  if (timeLeft <= 0) {
+    clearInterval(intervalId);
+    checkAnswer();
+  }
+};
+
+// start timer
+const startTimer = () => {
+  intervalId = setInterval(setTimeLeft, 1000);
+};
+
+// stop timer
+const stopTimer = () => {
+  clearInterval(intervalId);
+};
+
+// question change function
+const displayQuestions = () => {
+  question.textContent = `Questão ${currentQuestion + 1};`;
+  questionText.textContent = questions[currentQuestion].question;
+  for (i = 0; i < answerOptions.length; i++) {
+    answerOptions[i].value = questions[currentQuestion].options[i].correct;
+
+    answerOptions[i].nextSibling.textContent =
+      questions[currentQuestion].options[i].option;
+
+    answerOptions[i].checked = false;
+  }
+};
